eb34fd990f887a7ddd2123c344f4e4d55bd065dd
Alexey Chingaev <a.chingaev@ritm.media>
2024-07-12 23:22:33 +0300

    init

---
+++ vite.config.ts
@@ -0,0 +1,57 @@
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+import dns from "dns";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
+dns.setDefaultResultOrder("verbatim");
+
+// https://vitejs.dev/config/
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    react({
+      babel: {
+        plugins: [
+          "@emotion",
+          ["@babel/plugin-proposal-decorators", { legacy: true }],
+        ],
+      },
+    }),
+    basicSsl(),
+  ],
+  server:
+    process.env.NODE_ENV === "production"
+      ? undefined
+      : {
+          port: 3001,
+          cors: {
+            origin: true,
+          },
+          proxy: {
+            "/api": {
+              changeOrigin: true,
+              target: "http://localhost:3000",
+            },
+          },
+        },
+  build: {
+    outDir: "./docs",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: [
+            "node_modules/react/index.js",
+            "node_modules/react-dom/index.js",
+          ],
+        },
+      },
+    },
+  },
+  define: {
+    "process.env": process.env,
+  },
+  base: "./",
+});
