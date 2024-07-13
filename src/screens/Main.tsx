8134aa947163d9860c44275d73925214084e2b43
Alexey Chingaev <a.chingaev@ritm.media>
2024-07-13 11:10:38 +0300

    test

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import WebApp from "@twa-dev/sdk";
-import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+// import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 
 export const Main = () => {
-  const defaultState = {
-    center: [55.751574, 37.573856],
-    zoom: 5,
-  };
+  // const defaultState = {
+  //   center: [55.751574, 37.573856],
+  //   zoom: 5,
+  // };
   return (
     <div>
       <p>Главная</p>
       <button onClick={() => WebApp.showAlert(`Hello World! `)}>
         Show Alert
       </button>
-      <YMaps query={{ apikey: "cf9ae4bb-ef7b-4c05-acd6-cfa7ee09955e" }}>
+      {/* <YMaps query={{ apikey: "cf9ae4bb-ef7b-4c05-acd6-cfa7ee09955e" }}>
         <Map defaultState={defaultState}>
           <Placemark geometry={[55.684758, 37.738521]} />
         </Map>
-      </YMaps>
+      </YMaps> */}
     </div>
   );
 };
