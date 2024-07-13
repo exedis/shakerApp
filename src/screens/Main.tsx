196dc433034f8452d380ae2fb56d5a3ad1039d91
Alexey Chingaev <a.chingaev@ritm.media>
2024-07-13 15:00:02 +0300

    add map

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import WebApp from "@twa-dev/sdk";
-// import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+import { Link } from "react-router-dom";
+import { Path } from "@consts/path";
 
 export const Main = () => {
-  // const defaultState = {
-  //   center: [55.751574, 37.573856],
-  //   zoom: 5,
-  // };
+  const defaultState = {
+    center: [55.751574, 37.573856],
+    zoom: 5,
+  };
   return (
     <div>
       <p>Главная</p>
+      <Link to={Path.TO_EVENTS}>Перейти в список</Link>
       <button onClick={() => WebApp.showAlert(`Hello World! `)}>
         Show Alert
       </button>
-      {/* <YMaps query={{ apikey: "cf9ae4bb-ef7b-4c05-acd6-cfa7ee09955e" }}>
+      <YMaps query={{ apikey: "cf9ae4bb-ef7b-4c05-acd6-cfa7ee09955e" }}>
         <Map defaultState={defaultState}>
           <Placemark geometry={[55.684758, 37.738521]} />
         </Map>
-      </YMaps> */}
+      </YMaps>
     </div>
   );
 };
